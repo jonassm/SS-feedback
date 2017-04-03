71299145e18651aec790ac63f4671ecfc5bffea1
Jonassm <jonas-morbjerg@live.dk>
2017-04-03 17:29:46 +0200

    Added command

--- src/com/simplesurvival/feedback/feedback.java
+++ src/com/simplesurvival/feedback/feedback.java
@@ -1,15 +1,28 @@
 package com.simplesurvival.feedback;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
 public class feedback extends JavaPlugin {
 
-    // Fired when plugin is first enabled
+
     @Override
     public void onEnable() {
     }
-    // Fired when plugin is disabled
+
     @Override
     public void onDisable() {
 
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("feedback")) {
+            sender.sendMessage("Link: https://goo.gl/forms/i19zKKegrJ7d6cEf1");
+            return true;
+        }
+        return false;
+    }
+   
 }
